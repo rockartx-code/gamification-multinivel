e9194367c2820807bb5923e6e34b6df440ee4f46
rockartx-code <rock.artx@gmail.com>
2026-01-17 14:29:32 -0600

    Reinicio
    This reverts commit 31f7126adf2756dad7a7633caae29bdbafcf7633.

--- gamification-multinivel-front/src/app/mocks/metrics.fixture.ts
+++
@@ -1,8 +0,0 @@
-import { Metrics } from '../domain/models';
-
-export const metricsFixture: Metrics = {
-  impactPoints: 48250,
-  progressPoints: 8650,
-  newMembers: 18,
-  conversionRate: 12.4,
-};
