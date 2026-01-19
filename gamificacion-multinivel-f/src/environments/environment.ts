290093056f6068a9dc03145298ac40657da7f6cd
rockartx-code <rock.artx@gmail.com>
2026-01-18 20:52:48 -0600

    Update environment.ts

--- gamificacion-multinivel-f/src/environments/environment.ts
+++ gamificacion-multinivel-f/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  useMockApi: true,
-  apiBaseUrl: 'https://api.example.com'
+  useMockApi: false,
+  apiBaseUrl: 'https://m85v7secp8.execute-api.us-east-1.amazonaws.com/default/Multinivel'
 };
